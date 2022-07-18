5834948c0e149bbb62d72d11b12a5c141016cbf4
Yevhenii Nabok <1337niceshot@gmail.com>
2022-07-18 23:07:25 +0300

    Project initialized

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace homework_prac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
